cf033f67e177613f18a8801b25e02795b90cc716
Kevin Anantha <kevin.anantha@gmail.com>
2018-11-15 10:33:54 +0800

    :trollface:

--- src/condition.go
+++ src/condition.go
@@ -31,5 +31,84 @@ func main() {
   }
   */
 
+  // Nested if else
+  /*
+  point := 10
 
+  if point > 7 {
+    switch point {
+      case 10:
+        fmt.Println("Perfect")
+      default:
+        fmt.Println("Nice")
+    }
+  } else {
+    if point == 5 {
+      fmt.Println("not bad")
+    } else if point == 3 {
+      fmt.Println("keep trying")
+    } else {
+      fmt.Println("you can do it")
+      if point == 0 {
+        fmt.Println("try harder!")
+      }
+    }
+  }
+  */
+
+  // For
+  /*
+  for i := 0; i < 5; i++ {
+    fmt.Println(i)
+  }
+  */
+
+  // For with condition (argument)
+  /*
+  i := 0
+
+  for i < 5 {
+    fmt.Println(i)
+    i++
+  }
+  */
+
+  // For without argument
+  /*
+  i := 0
+
+  for {
+    fmt.Println(i)
+
+    i++
+    if i == 5 {
+      break
+    }
+  }
+  */
+
+  // Break and continue
+  /*
+  for i:= 1; i <= 10; i++ {
+    if i % 2 == 1 {
+      continue
+    }
+
+    if i > 8 {
+      break
+    }
+
+    fmt.Println(i)
+  }
+  */
+
+  // Nested for
+  /*
+  for i := 0; i < 5; i++ {
+    for j := i; j < 5; j ++ {
+      fmt.Print(j, " ")
+    }
+    fmt.Println()
+  }
+  */
 }
