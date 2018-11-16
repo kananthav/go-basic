35100428a757e3efdfa13a170b33fa30b96121d4
Kevin Anantha <kevin.anantha@gmail.com>
2018-11-16 19:53:55 +0800

    :trollface:

---
+++ src/array.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func main() {
+  /*
+  var names[4] string
+  names[0] = "trafalgar"
+  names[1] = "d"
+  names[2] = "water"
+  names[3] = "law"
+  fmt.Println(names[0], names[1], names[2], names[3])
+  */
+
+  /*
+  fruits := [4] string {
+    "apple",
+    "grape",
+    "banana",
+    "melon",
+  }
+  fmt.Println("Jumlah element \t\t", len(fruits))
+  fmt.Println("Isi semua element \t", fruits)
+  */
+
+  /*
+  numbers := [...] int {
+    1,
+    2,
+    3,
+    4,
+  }
+
+  fmt.Println("data array \t:", numbers)
+  fmt.Println("jumlah elemen \t:", len(numbers))
+  */
+
+  /*
+  var numbers1 = [2][3]int{[3]int{3, 2, 3}, [3]int{3, 4, 5}}
+  var numbers2 = [2][3]int{{3, 2, 3}, {3, 4, 5}}
+  fmt.Println("numbers1", numbers1)
+  fmt.Println("numbers2", numbers2)
+  */
+
+  /*
+  var fruits = [4]string{"apple", "grape", "banana", "melon"}
+  for i := 0; i < len(fruits); i++ {
+    fmt.Printf("elemen %d : %s\n", i, fruits[i])
+  }
+  */
+
+  /*
+  var fruits = [4]string{"apple", "grape", "banana", "melon"}
+  for i, fruit := range fruits {
+    fmt.Printf("elemen %d : %s\n", i, fruit)
+  }
+  */
+}
